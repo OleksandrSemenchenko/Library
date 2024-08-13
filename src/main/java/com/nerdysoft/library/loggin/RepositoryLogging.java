969bc8a61ab5f1c059ee5342382452479075b935
Oleksandr Semenchenko <477163@gmail.com>
2024-08-13 13:56:35 +0300

    [NS-1] Add logging for repository methods

---
+++ src/main/java/com/nerdysoft/library/loggin/RepositoryLogging.java
@@ -0,0 +1,30 @@
+package com.nerdysoft.library.loggin;
+
+import java.util.Arrays;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class RepositoryLogging {
+
+  @Pointcut("execution(public * com.nerdysoft.library.repository.*Repository.*(..))")
+  void repositoryMethod() {}
+
+  @Around("repositoryMethod() ")
+  public Object addLoggingToRepositoryMethods(ProceedingJoinPoint joinPoint) throws Throwable {
+    String className = joinPoint.getSignature().getDeclaringTypeName();
+    String methodName = joinPoint.getSignature().getName();
+    String arguments = Arrays.toString(joinPoint.getArgs());
+    log.warn(String.format("Request: %s.%s.s.(%s)", className, methodName, arguments));
+    Object result = joinPoint.proceed();
+    log.warn(
+        String.format(
+            "Response: %s.%s.(%s) Returned: %s", className, methodName, arguments, result));
+    return result;
+  }
+}
