bee0e62e8bc3f65ab1f5e6d8a2acdf6a643b5a73
Oleksandr Semenchenko <477163@gmail.com>
2024-08-13 14:55:49 +0300

    [NS-1] Add test for the controller

---
+++ src/test/java/com/nerdysoft/library/controller/UserControllerTest.java
@@ -0,0 +1,45 @@
+package com.nerdysoft.library.controller;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.nerdysoft.library.exceptionhandler.exceptions.UserNotFoundException;
+import com.nerdysoft.library.service.UserService;
+import java.util.UUID;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@WebMvcTest(UserController.class)
+@AutoConfigureMockMvc
+class UserControllerTest {
+
+  private static final String V1 = "/v1";
+  private static final String USER_ID_PATH = "/users/{userId}";
+  private static final UUID NOT_EXISTING_USER_ID = UUID.randomUUID();
+
+  @Autowired private MockMvc mockMvc;
+
+  @MockBean private UserService userService;
+
+  @Test
+  void getUser_shouldReturnStatus404_whenNoUserInDb() throws Exception {
+    when(userService.getUser(NOT_EXISTING_USER_ID)).thenThrow(UserNotFoundException.class);
+
+    mockMvc
+        .perform(
+            MockMvcRequestBuilders.get(V1 + USER_ID_PATH, NOT_EXISTING_USER_ID)
+                .contentType(APPLICATION_JSON))
+        .andExpect(status().isNotFound())
+        .andExpect(jsonPath("$.details").hasJsonPath())
+        .andExpect(jsonPath("$.errorCode", Matchers.is(404)))
+        .andExpect(jsonPath("$.timestamp").exists());
+  }
+}
